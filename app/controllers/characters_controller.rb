3de47fc7215eee31584fd90a09af3830b4717cfe
andres ibanez <andresibanezulloa@gmail.com>
2022-12-07 19:52:26 -0300

    controller actions

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,16 +1,30 @@
 class CharactersController < ApplicationController
+  before_action :set_character, only: %i[show update destroy]
   def index
+    characters = Character.all
   end
-
   def show
+    
   end
 
   def create
+    character = Character.new(character_params)
   end
 
   def update
+    character.update(character_params)
   end
 
   def destroy
+    character.destroy
+  end
+
+  private
+  def set_character
+    character = Character.find(params[:id])
+  end
+
+  def character_params
+    params.require(:character).permit(:name, :actor, :review, :image_url)
   end
 end
